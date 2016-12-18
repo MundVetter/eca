4d38c508f4337277120c16d48ab74b685af8cfda
MundVetter <mundvetter@gmail.com>
2016-12-18 21:30:50 +0100

    naming change
    from row and line to lattice

--- eca/README.md
+++ eca/README.md
@@ -7,17 +7,17 @@ the second parameter is optional. The second parameter is an object. That object
 can contain a `seed`. The `seed` is a binary string. The `seed` defaults to `'1'`.
 You can also determine the `width` of the eca. The `width` property defaults to
 `11`.
-With the `.genLine()` method you generate a new line of your eca. All the generated lines and original seed are
-saved in an array in the `.rows` property.
+With the `.genLattice()` method you generate a new line of your eca. All the generated lines and original seed are
+saved in an array in the `.lattices` property.
 
 example:
 ``` javascript
 const eca = require('eca')
 
 let thirty = new eca(30)
-thirty.genLine()
-thirty.genLine()
-thirty.genLine()
-thirty.rows
+thirty.genLattice()
+thirty.genLattice()
+thirty.genLattice()
+thirty.lattices
 // => [ '00000100000', '00001110000', '00011001000', '00110111100' ]
 ```
--- eca/index.js
+++ eca/index.js
@@ -23,45 +23,45 @@ class eca {
     ]
     this.RESULTS = this.rule(number)
 
-    this.rows = []
+    this.lattices = []
     this.newRow = ''
     this.initialRow()
   }
   rule(number) {
     return this.leftPad(number.toString(2), 8, 0)
   }
-  //Generates the initial row from a seed
+  //Generates the initial lattice from a seed
   initialRow() {
-    this.rows.push(this.seed)
+    this.lattices.push(this.seed)
     let margin = (this.width - this.seed.length) / 2
 
     if (margin % 1 != 0) {
-      this.rows[0] += '0'
+      this.lattices[0] += '0'
       margin = Math.floor(margin)
     }
     for (let i = 0; i < margin; i++) {
-      this.rows[0] += '0'
+      this.lattices[0] += '0'
     }
     for (let i = 0; i < margin; i++) {
-      this.rows[0] = '0' + this.rows[0]
+      this.lattices[0] = '0' + this.lattices[0]
     }
   }
-  // generates a new line and pushes the line into the rows
-  genLine() {
-    const CURR_ROW = this.rows[this.rows.length - 1]
-    for (let i = 0; i < CURR_ROW.length; i++) {
+  // generates a new line and pushes the line into the lattices
+  genlattice() {
+    const CURR_LATTICE = this.lattices[this.lattices.length - 1]
+    for (let i = 0; i < CURR_LATTICE.length; i++) {
       let a = i - 1
       let b = i + 2
 
-      let slice = CURR_ROW.slice(a, b)
+      let slice = CURR_LATTICE.slice(a, b)
       // on the edge get the cell of the other side
       if(a < 0) {
-        let begin = CURR_ROW.slice(a)
-        let end = CURR_ROW.slice(0, b)
+        let begin = CURR_LATTICE.slice(a)
+        let end = CURR_LATTICE.slice(0, b)
         slice = begin + end
-      } else if(b > CURR_ROW.length) {
-        let begin  = CURR_ROW.slice(a)
-        let end = CURR_ROW.slice(0, b - CURR_ROW.length)
+      } else if(b > CURR_LATTICE.length) {
+        let begin  = CURR_LATTICE.slice(a)
+        let end = CURR_LATTICE.slice(0, b - CURR_LATTICE.length)
         slice = begin + end
       }
       for (let i = 0; i < this.PATTERNS.length; i++) {
@@ -71,7 +71,7 @@ class eca {
         }
       }
     }
-    this.rows.push(this.newRow)
+    this.lattices.push(this.newRow)
     this.newRow = ''
   }
 }
