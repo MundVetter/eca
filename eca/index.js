5f3afa23d17fa2ef76fa1be57ef18c033fcdc45c
Mund <MundVetter@users.noreply.github.com>
2017-01-04 13:09:09 +0100

    bug fix

--- eca/index.js
+++ eca/index.js
@@ -30,7 +30,7 @@ class eca {
     this._initialLattice()
   }
   _rule(number) {
-    if(number < 0 || number > 255)
+    if(number <= 0 || number >= 255)
       throw new Error(number + ' is not a rule!')
     return leftPad(number.toString(2), 8, 0)
   }
