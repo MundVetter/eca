4ee28356f3404d06846810f4f5eca70ceb2edc22
MundVetter <mundvetter@gmail.com>
2017-06-07 02:03:00 +0200

    a mininamilist version of eca

--- index.js
+++ index.js
@@ -1,59 +1,24 @@
 /*
-  Made by Mund & Mart for their profielwerkstuk to show a possible js implementation of
-  ellementary cellular automata
+a mininamilist version of eca
 */
 'use strict'
-
 const assert = require('assert');
-class Eca {
-  constructor(number, options = {}) {
-    this.neighbors = options.neighbors || 2
-    this.states = options.states || 2
-    assert.equal(typeof this.neighbors && typeof this.states, 'number')
-
-    this.patterns = Math.pow(this.states, this.neighbors + 1)
-
-    this.lattices = []
-    this.results = this._rule(number)
-    this._initialLattice(options.seed, options.width)
+function genLattice(lattice, rule, states = 2, neighbors = 2) {
+  assert.equal(typeof lattice, 'string')
+  assert.equal(typeof rule && typeof state && typeof neighbors, 'number')
+  const patterns = Math.pow(states, neighbors + 1)
+  const max = Math.pow(states, patterns) - 1
+  assert.ok(rule >= 0 && rule <= max)
+  const lookup = rule.toString(states).padStart(patterns, 0)
+  let newLattice = ''
+  for(let i = 0; i < lattice.length; i++) {
+    const a = i - neighbors / 2
+    const b = i + 1 + neighbors / 2
+    const neighborhood = lattice.substring(a, b)
+    const index = lookup.length - (parseInt(neighborhood, states) + 1)
+    newLattice += lookup.charAt(index)
   }
-  _rule(number = 30) {
-    assert.equal(typeof number, 'number')
-    const max = Math.pow(this.states, this.patterns) - 1
-    assert.ok(number >= 0 && number <= max)
-    return number.toString(this.states).padStart(this.patterns, 0)
-  }
-  //Generates the initial lattice from a seed
-  _initialLattice(seed = '1', width = 11) {
-    assert.ok(seed.length <= width)
-    assert.equal(typeof seed, 'string')
-
-    this.lattices.push(seed)
-    let margin = (width - seed.length) / 2
-    if(margin % 1 != 0) {
-      this.lattices[0] += '0'
-      margin = Math.floor(margin)
-    }
-    for(let i = 0; i < margin; i++) {
-      this.lattices[0] += '0'
-      this.lattices[0] = '0' + this.lattices[0]
-    }
-  }
-  genLattice() {
-    let newLattice = ''
-    const lattice = this.lattices[this.lattices.length - 1]
-    for(let i = 0; i < lattice.length; i++) {
-      const a = i - this.neighbors / 2
-      const b = i + 1 + this.neighbors / 2
-      const neighborhood = lattice.substring(a, b)
-      const index = this.results.length - (parseInt(neighborhood, this.states) + 1)
-      newLattice += this.results.charAt(index)
-    }
-    this.lattices.push(newLattice)
-    return newLattice
-  }
+  return newLattice
 }
-function eca(number, options = {}) {
-  return new Eca(number, options)
-}
-module.exports = eca
+
+module.exports = genLattice
--- test.js
+++ test.js
@@ -1,31 +1,8 @@
 'use strict'
-const eca = require('./')
+const genLattice = require('./')
 const test = require('tape')
 
 test('genLattice', (assert) => {
-  assert.plan(3)
-  let thirty = eca(30)
-  thirty.genLattice()
-  thirty.genLattice()
-  thirty.genLattice()
-  assert.deepEqual(thirty.lattices, ['00000100000', '00001110000',
-  '00011001000', '00110111100'])
-
-  let even = eca(30, {width: 4})
-  even.genLattice()
-  even.genLattice()
-  assert.equal(even.lattices[2].length, 4)
-  assert.equal(even.lattices[0], '0100')
-})
-test('_initialLattice', (assert) => {
-  assert.plan(2)
-  assert.throws(() => eca(30, {width: 3, seed: '1111'}))
-  assert.throws(() => eca(1, {seed: [1]}))
-})
-test('_rule', (assert) => {
-  assert.plan(3)
-  let env = eca(1)
-  assert.equal(env.results, '00000001')
-  assert.throws(() => env._rule(-1))
-  assert.throws(() => env._rule('1'))
+  assert.plan(1)
+  assert.equal(genLattice('00000100000', 30), '00001110000')
 })
